160360d88c4ca45d31d15a3a98219f6b4b586f7f
mohax <mohax.spb@gmail.com>
2019-09-18 23:11:14 +0300

    Add ngVar directive. Add code, which fails while prod build.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    '<div *ngVar="title as ngVarTitle">' +
+    '<h3>{{ngVarTitle}}</h3>' +
+    '<button (click)="ngVarTitle=changedTitle">click me!</button>' +
+    '</div>'
 })
 export class AppComponent {
+  // noinspection JSUnusedGlobalSymbols
   title = 'ng-var-directive-issue';
+  // noinspection JSUnusedGlobalSymbols
+  changedTitle = 'Title changed!';
+}
+
+/**
+ * see https://stackoverflow.com/a/43172992/3212712
+ */
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[ngVar]',
+})
+export class VarDirective {
+  @Input()
+  set ngVar(context: any) {
+    this.context.$implicit = this.context.ngVar = context;
+    this.updateView();
+  }
+
+  context: any = {};
+
+  constructor(private vcRef: ViewContainerRef, private templateRef: TemplateRef<any>) {
+  }
+
+  updateView() {
+    this.vcRef.clear();
+    this.vcRef.createEmbeddedView(this.templateRef, this.context);
+  }
 }
